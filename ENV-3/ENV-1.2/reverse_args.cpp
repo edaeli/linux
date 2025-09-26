aafcb37eab0ed06215f5d0e0d0fc2e3536997914
edaeli <ayvazyankhoren@gmail.com>
2025-09-26 23:55:10 +0400

    Delete ENV-3/ENV-1.2 directory

--- ENV-3/ENV-1.2/reverse_args.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    for (int i = argc - 1; i >= 1; i--) {
-        std::cout << argv[i] << " ";
-    }
-    std::cout << std::endl;
-    return 0;
-}
